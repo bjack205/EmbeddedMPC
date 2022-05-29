6c73b0459f377e7ec769eb9f40bd6949f54f8365
Brian Jackson <bjack205@gmail.com>
2022-05-29 19:03:52 -0400

    Working single solve

--- examples/doubleintegrator/doubleintegrator.ino
+++ examples/doubleintegrator/doubleintegrator.ino
@@ -1,21 +1,29 @@
 #include <ArduinoEigenDense.h>
 
 #include <EmbeddedMPC.h>
-// #include "problemdata.hpp"
-#include "problem.hpp"
-// #include "osqp.h"
-// #include "qdldl.h"
-// #include "qdldl_types.h"
+#include "workspace.h"
 
 #define LED_PIN 13
 
 void setup() {
     pinMode(LED_PIN, OUTPUT);
+    while (!Serial) { delay(10); }
+    Serial.println("Connected to Teensy!");
+    uint64_t tstart = micros();
+    osqp_solve(&workspace);
+    uint64_t tsolve_us = micros() - tstart;
+    float tsolve_ms = tsolve_us / 1000.0;
+    Serial.print("Solve took "); Serial.print(tsolve_ms, 4); Serial.println(" ms");
+    Serial.print("Status:                "); Serial.println((&workspace)->info->status);
+    Serial.print("Number of iterations:  "); Serial.println((int)((&workspace)->info->iter));
+    Serial.print("Objective value:       "); Serial.println((&workspace)->info->obj_val, 4);
+    Serial.print("Primal residual:       "); Serial.println((&workspace)->info->pri_res, 4);
+    Serial.print("Dual residual:         "); Serial.println((&workspace)->info->dua_res, 4);
 }
 
 void loop() {
     digitalWrite(LED_PIN, HIGH);
-    delay(500);
+    delay(200);
     digitalWrite(LED_PIN, LOW);
-    delay(500);
+    delay(200);
 }
--- test/doubleintegrator.h
+++ test/doubleintegrator.h
@@ -9,13 +9,22 @@ const float b = h * h / 2.0;
 const float Qfdata[4] = {
   10.0, 10.0, 10.0, 10.0
 };
+const float qfdata[4] = {
+  0,0,0,0
+};
 
 const float Qdata[4] = {
   1.0, 1.0, 1.0, 1.0
 };
+const float qdata[4] = {
+  0,0,0,0
+};
 
 const float Rdata[2] = {
   1e-2, 1e-2
+};
+const float rdata[2] = {
+  0,0
 };
 
 const float Adata[16] = {  // NOLINT
